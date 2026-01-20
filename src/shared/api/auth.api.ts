9813c82bfd16ac4bdfaae3fb724e79c926d23fd2
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-01-20 17:14:35 +0300

    feat(auth): add auth api

---
+++ src/shared/api/auth.api.ts
@@ -0,0 +1,50 @@
+import { api } from './axios';
+import type {User} from "../../entities/user/model/user.types.ts";
+import type {LoginDto} from "../../features/auth/login/model/login.dto.ts";
+import type {RegisterDto} from "../../features/auth/register/model/register.dto.ts";
+
+interface AuthResponse {
+  user: User;
+  accessToken: string;
+}
+
+interface RefreshResponse {
+  accessToken: string;
+}
+
+interface MeResponse {
+  userId: string;
+  email: string;
+}
+
+export const authApi = {
+  async login(dto: LoginDto): Promise<AuthResponse> {
+    const { data } = await api.post<AuthResponse>('/auth/login', dto);
+    return {
+      user: data.user,
+      accessToken: data.accessToken,
+    };
+  },
+
+  async register(dto: RegisterDto): Promise<AuthResponse> {
+    const { data } = await api.post<AuthResponse>('/auth/register', dto);
+    return {
+      user: data.user,
+      accessToken: data.accessToken,
+    };
+  },
+
+  async refresh(): Promise<RefreshResponse> {
+    const { data } = await api.post<RefreshResponse>('/auth/refresh');
+    return data;
+  },
+
+  async logout(): Promise<void> {
+    await api.post('/auth/logout');
+  },
+
+  async me(): Promise<MeResponse> {
+    const { data } = await api.get<MeResponse>('/auth/me');
+    return data;
+  },
+};
