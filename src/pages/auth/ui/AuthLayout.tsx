5df556e5e45e7d36e15a86e1a4d9a3d41bf03110
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-01-27 21:25:52 +0300

    feat(style): add auth layout basic styles structure

--- src/pages/auth/ui/AuthLayout.tsx
+++ src/pages/auth/ui/AuthLayout.tsx
@@ -1,31 +1,32 @@
 import { Outlet, useLocation, Link } from 'react-router-dom';
+import s from './AuthLayout.module.scss'
 
 export function AuthLayout() {
   const location = useLocation();
   const isLogin = location.pathname.includes('login');
 
   return (
-    <div>
-      <div>
-        {isLogin ? (
-          <div>
-            <h2>Нет аккаунта?</h2>
-            <Link to="/register">
-              Зарегистрироваться
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <h2>Уже есть аккаунт?</h2>
-            <Link to="/login">
-              Войти
-            </Link>
-          </div>
-        )}
-      </div>
+    <div className={s.authContainer}>
+      <Outlet />
 
       <div>
-        <Outlet />
+        {isLogin ? (
+          <div className={s.container}>
+            <h2 className={s.header}>Don’t have an Account Yet?</h2>
+            <p className={s.paragraph}>
+              Let’s get you all set up so you can start creating your first onboarding experience
+            </p>
+            <Link className={s.link} to="/register">Register</Link>
+          </div>
+        ) : (
+          <div className={s.container}>
+            <h2 className={s.header}>Already Signed up?</h2>
+            <p className={s.paragraph}>
+              Log in to your account so you can continue building and editing your onboarding flows
+            </p>
+            <Link className={s.link} to="/login">Log in</Link>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,9 +1,10 @@
 import { LoginForm } from '@/features/auth/login';
+import s from './LoginPage.module.scss';
 
 export function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={s.container}>
+      <h2 className={s.header}>Log in to Your Account</h2>
       <LoginForm />
     </div>
   );
