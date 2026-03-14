38a987e5a7baa889d6412ea768087a0678490b25
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-03-14 15:37:24 +0300

    refactor(styles): refactor nav styles

--- src/shared/ui/page-home-link/PageHomeLink.tsx
+++ src/shared/ui/page-home-link/PageHomeLink.tsx
@@ -1,10 +1,5 @@
-import { linkStyles } from '@/shared/lib/styles/link.styles.ts';
 import { LinkAsButton } from '@/shared/ui';
 
 export function PageHomeLink() {
-  return (
-      <LinkAsButton to={'/'} style={linkStyles}>
-        Home
-      </LinkAsButton>
-  );
+  return <LinkAsButton to={'/'}>Home</LinkAsButton>;
 }
