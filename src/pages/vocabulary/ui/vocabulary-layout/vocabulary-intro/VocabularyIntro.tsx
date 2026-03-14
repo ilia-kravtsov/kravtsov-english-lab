031a6ff5e1edbdb071ca4f06091e92a355fed1c2
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-03-14 18:05:20 +0300

    refactor(style): change vocabulary styles

--- src/pages/vocabulary/ui/vocabulary-layout/vocabulary-intro/VocabularyIntro.tsx
+++ src/pages/vocabulary/ui/vocabulary-layout/vocabulary-intro/VocabularyIntro.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
+import { LinkAsButton } from '@/shared/ui';
+
 import style from './VocabularyIntro.module.scss';
 
 export function VocabularyIntro() {
@@ -15,17 +17,17 @@ export function VocabularyIntro() {
           To get started, choose one of the options from the menu above
         </p>
         <p className={style.paragraph}>
-          <NavLink to="cards">Cards — </NavLink>
-          ready-made sets of cards organized by difficulty level create your own cards for
+          <NavLink to={"cards"}>Cards — </NavLink>
+          ready-made sets of cards organized by difficulty level. Create your own cards for
           personalized practice
         </p>
         <p className={style.paragraph}>
-          <NavLink to="words-bank">Words bank — </NavLink>
+          <NavLink to={"words-bank"}>Words bank — </NavLink>
           save unfamiliar words and new expressions for later review and future practice
         </p>
       </div>
 
-      <h3 className={style.subtitle}>Let's dive in!</h3>
+      <LinkAsButton to={"cards"}>Let's dive in!</LinkAsButton>
     </div>
   );
 }
