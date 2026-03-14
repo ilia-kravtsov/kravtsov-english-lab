82e020bfe949087ef7330994b56b3d4b7cf25805
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-03-14 20:45:57 +0300

    refactor(style): change card sets styles

--- src/features/vocabulary/card-sets/ui/card-sets-list-item/CardSetsListItem.tsx
+++ src/features/vocabulary/card-sets/ui/card-sets-list-item/CardSetsListItem.tsx
@@ -1,8 +1,6 @@
 import type { KeyboardEvent } from 'react';
 
 import type { useCardSets } from '@/features/vocabulary/card-sets/model/use-card-sets.ts';
-import { Button } from '@/shared/ui';
-import { mediumButtonStyles } from '@/shared/lib/styles/button.styles.ts';
 
 import style from '../CardSets.module.scss';
 
@@ -17,13 +15,7 @@ type Props = {
   onDelete: () => void;
 };
 
-export function CardSetsListItem({
-                                   item,
-                                   active,
-                                   onOpen,
-                                   onEdit,
-                                   onDelete,
-                                 }: Props) {
+export function CardSetsListItem({ item, active, onOpen, onEdit, onDelete }: Props) {
   function handleKeyDown(e: KeyboardEvent<HTMLDivElement>) {
     if (e.key === 'Enter' || e.key === ' ') {
       e.preventDefault();
@@ -44,8 +36,13 @@ export function CardSetsListItem({
       <div className={style.itemCount}>{item.cardsCount} cards</div>
 
       <div className={style.itemActions} onClick={(e) => e.stopPropagation()}>
-        <Button title={'Edit'} onClick={onEdit} style={mediumButtonStyles} />
-        <Button title={'Delete'} onClick={onDelete} style={mediumButtonStyles} />
+        <button className={style.iconButton} onClick={onEdit} aria-label={'Edit card set'}>
+          ✏️
+        </button>
+
+        <button className={style.iconButton} onClick={onDelete} aria-label={'Delete card set'}>
+          🗑
+        </button>
       </div>
     </div>
   );
